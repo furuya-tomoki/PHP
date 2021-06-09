17ac239f0b16e693765f9530b6b8e39f6db20c7c
furuya-tomoki <furuya11221993@gmail.com>
2021-06-09 08:15:01 +0000

    [Add]rand.php

---
+++ rand.php
@@ -0,0 +1,5 @@
+<?php
+// Here your code !
+$rand_num = rand(50,99);
+echo "スライムに".$rand_num."のダメージを与えた。";
+?>
---
+++ variable.php
@@ -0,0 +1,12 @@
+<?php
+$text1 = "魔物が";
+$text2 = "あらわれた";
+echo $text1.$text2;
+
+?>
+
+<?php
+$damage = 12;
+echo "スライムの攻撃。".$damage."のダメージを受けた";
+
+?>
