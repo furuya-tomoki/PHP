210a29ac6e91326ed768617779922ecb01c186f9
furuya-tomoki <furuya11221993@gmail.com>
2021-06-11 13:08:22 +0000

    [Finish]if3.php

---
+++ if2.php
@@ -0,0 +1,18 @@
+<?php
+$place = rand(1,5); // 順位を1〜5の範囲でランダムに作られた数字を$placeに代入
+echo "placeの中身:".$place."\n";
+if($place == 1){
+    // 1位だったときの処理
+    echo "金賞";
+}elseif($place == 2){
+    // 複数の条件を定義
+    echo "銀賞";
+}elseif($place == 3){
+     // 複数の条件を定義
+    echo "銅賞";
+}else{
+    // それ以外だったときの処理
+    echo $place."位";
+}
+?>
+
