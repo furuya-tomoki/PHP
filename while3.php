5f5bf6250b688f24cf522ba4e5d4d744e0ff09af
furuya-tomoki <furuya11221993@gmail.com>
2021-06-13 10:48:26 +0000

    [Finish]while3.php

---
+++ while3.php
@@ -0,0 +1,26 @@
+<?php
+$i = 1;
+while($i <= 20){
+ echo $i."\n";
+ $i++;
+}
+?>
+
+<?php
+$input1 = trim(fgets(STDIN));
+$input2 = trim(fgets(STDIN));
+while($input1 <= $input2){
+    echo $input1."\n";
+    $input1++;
+}
+?>
+
+<?php
+$input = trim(fgets(STDIN));
+$i=1;
+while($i <= $input){
+    echo "眠い。\n";
+    $i++;
+}
+
+?>
