9b4aac8342aa831a48ad9cf5f19feeaeffc328b4
furuya-tomoki <furuya11221993@gmail.com>
2021-06-17 14:03:01 +0000

    [Add]foreach

---
+++ foreach.php
@@ -0,0 +1,8 @@
+<?php
+$item = array("ショートソード", "ロングソード", "ブレードソード", "バスタードソード");
+// foreach ループで $item 配列の value 全てを一つずつ
+// \n を末尾に付けて改行しながら出力してみましょう。
+foreach($item as $value){
+    echo $value."\n";
+}
+?>
