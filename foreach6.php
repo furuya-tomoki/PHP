468492b5dc09e5ba45611b323e684a78056c63be
furuya-tomoki <furuya11221993@gmail.com>
2021-06-19 14:09:27 +0000

    [Add]foreach6,php

---
+++ foreach6.php
@@ -0,0 +1,27 @@
+<?php
+$item_img = array(
+    "剣" => "http://paiza.jp/learning/images/sword.png",
+    "盾" => "http://paiza.jp/learning/images/shield.png",
+    "回復薬" => "http://paiza.jp/learning/images/potion.png",
+    "クリスタル" => "http://paiza.jp/learning/images/crystal.png"
+);
+// ここから下に記述
+foreach($item_img as $value){
+    echo "<img src=".$value."><br>\n";
+}
+?>
+
+<?php
+$item_img = array(
+    "剣" => "http://paiza.jp/learning/images/sword.png",
+    "盾" => "http://paiza.jp/learning/images/shield.png",
+    "回復薬" => "http://paiza.jp/learning/images/potion.png",
+    "クリスタル" => "http://paiza.jp/learning/images/crystal.png"
+);
+// ここから下に記述
+while($input = fgets(STDIN)){
+    $input = trim($input);
+    echo "<img src=".$item_img[$input].">";
+}
+
+?>
